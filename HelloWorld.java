6bc5269f000bd5143f13e54cd1c11e68705957de
JonnyRicotto <40657675+JonnyRicotto@users.noreply.github.com>
2018-08-21 15:33:40 +0000

    Add files via upload
    New hello

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+package com.acn;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HelloWorld hello = new HelloWorld();
+		hello.showHello(args);
+	}
+
+	private void showHello(String[] args) {
+		String name = (args.length > 0) ? args[0] : "anonymous";
+		System.out.println("Hello  " + name);
+	}
+
+}
